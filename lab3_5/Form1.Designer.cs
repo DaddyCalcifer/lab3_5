7001c55cad23294bc81997cb0e1e1e85188b365f
Никита Черничкин <nikita.macarov@yandex.ru>
2022-09-29 00:11:09 +0300

    Добавьте файлы проекта.

---
+++ lab3_5/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace lab3_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EPS_enter = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.result_output = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.EPS_enter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result_output)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EPS_enter
+            // 
+            this.EPS_enter.DecimalPlaces = 2;
+            this.EPS_enter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EPS_enter.Increment = new decimal(new int[] {
+            15,
+            0,
+            0,
+            131072});
+            this.EPS_enter.Location = new System.Drawing.Point(65, 12);
+            this.EPS_enter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.EPS_enter.Name = "EPS_enter";
+            this.EPS_enter.Size = new System.Drawing.Size(175, 29);
+            this.EPS_enter.TabIndex = 0;
+            this.EPS_enter.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            65536});
+            this.EPS_enter.ValueChanged += new System.EventHandler(this.EPS_enter_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lab3_5.Properties.Resources._3;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(228, 67);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "EPS";
+            // 
+            // result_output
+            // 
+            this.result_output.DecimalPlaces = 2;
+            this.result_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result_output.Location = new System.Drawing.Point(118, 120);
+            this.result_output.Name = "result_output";
+            this.result_output.ReadOnly = true;
+            this.result_output.Size = new System.Drawing.Size(122, 29);
+            this.result_output.TabIndex = 3;
+            this.result_output.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.result_output.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(251, 155);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.result_output);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.EPS_enter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Лабораторная работа 3";
+            ((System.ComponentModel.ISupportInitialize)(this.EPS_enter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result_output)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown EPS_enter;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown result_output;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab3_5/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result_output.Value = Convert.ToDecimal(func());
+        }
+        public int factorial(int n)
+        {
+            if (n == 0) return 0;
+            if(n ==1) return 1;
+            return n * factorial(n - 1);
+        }
+        public double func()
+        {
+            double eps = Convert.ToDouble(EPS_enter.Value);
+            double res = 0;
+            for (int i = 1; ; i++)
+            {
+                double tempo = 1 / Math.Sqrt(factorial(i));
+                if (tempo < eps) return res;
+                res += tempo;
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EPS_enter_ValueChanged(object sender, EventArgs e)
+        {
+            result_output.Value = Convert.ToDecimal(func());
+        }
+    }
+}
